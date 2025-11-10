9d35b59386c5f366be8d0564c2146f6848833ed9
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2025-11-10 17:48:41 +0300

    refactoring code

--- AuthorizationAPI/Authorization.Application/Common/Services/Interfaces/IUserRepository.cs
+++ AuthorizationAPI/Authorization.Application/Common/Services/Interfaces/IUserRepository.cs
@@ -4,9 +4,9 @@ namespace Authorization.Application.Common.Services.Interfaces;
 
 public interface IUserRepository
 {
-    Task<User> FindByEmailAsync(string email);
-    Task<User> GetAsync(int id);
-    Task<User> CreateAsync(User newUser);
-    Task<User> UpdateAsync(User user);
-    Task DeleteAsync(User user);
+    Task<User> FindByEmailAsync(string email, CancellationToken cancellationToken);
+    Task<User> GetAsync(Guid id, CancellationToken cancellationToken);
+    Task<User> CreateAsync(User newUser, CancellationToken cancellationToken);
+    Task<User> UpdateAsync(User user, CancellationToken cancellationToken);
+    Task DeleteAsync(User user, CancellationToken cancellationToken);
 }
--- AuthorizationAPI/Authorization.Persistence/Repositories/UserRepository.cs
+++ AuthorizationAPI/Authorization.Persistence/Repositories/UserRepository.cs
@@ -6,42 +6,41 @@ namespace Authorization.Persistence.Repositories;
 
 internal class UserRepository : IUserRepository
 {
-    private readonly IApplicationDbContext _db;
+    private readonly ApplicationDbContext _db;
     private readonly DbSet<User> _users;
 
-    public UserRepository(IApplicationDbContext db)
+    public UserRepository(ApplicationDbContext db)
     {
         _db = db;
         _users = _db.Set<User>();
     }
-    public async Task<User> CreateAsync(User user)
+    public async Task<User> CreateAsync(User user, CancellationToken cancellationToken)
     {
         _users.Add(user);
         await _db.SaveChangesAsync();
         return user;
     }
 
-    public async Task DeleteAsync(User user)
+    public async Task DeleteAsync(User user, CancellationToken cancellationToken)
     {
         _users.Remove(user);
         await _db.SaveChangesAsync();
     }
 
-    public async Task<User> FindByEmailAsync(string email)
+    public async Task<User> FindByEmailAsync(string email, CancellationToken cancellationToken)
     {
         return await _users
             .AsNoTracking()
             .FirstOrDefaultAsync(x => x.Email == email);
     }
 
-    public async Task<User> GetAsync(int id)
+    public async Task<User> GetAsync(Guid id, CancellationToken cancellationToken)
     {
         return await _users
-            .AsNoTracking()
             .FirstOrDefaultAsync(x => x.Id == id);
     }
 
-    public async Task<User> UpdateAsync(User user)
+    public async Task<User> UpdateAsync(User user, CancellationToken cancellationToken)
     {
         _users.Update(user);
         await _db.SaveChangesAsync();
