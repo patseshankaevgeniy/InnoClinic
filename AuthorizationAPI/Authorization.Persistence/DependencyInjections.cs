6e87e42b04325027beab10626165fa2e06bae8b9
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2025-11-10 18:03:23 +0300

    add Persistence DI

---
+++ AuthorizationAPI/Authorization.Persistence/DependencyInjections.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Authorization.Persistence;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddPersistenceDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+         .AddScoped(provider => provider.GetRequiredService<ApplicationDbContext>())
+         .AddDbContext<ApplicationDbContext>(options =>
+         {
+             var connectionString = configuration.GetConnectionString("DefaultConnection");
+             options.UseSqlServer(connectionString);
+             options.EnableSensitiveDataLogging();
+         });
+
+        return services;
+    }
+}
