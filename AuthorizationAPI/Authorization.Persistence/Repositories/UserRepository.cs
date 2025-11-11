d6e8b278becb1ebc3c8dd93737dbbfbff281374a
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2025-11-11 10:54:53 +0300

    set default for cancellationToken

--- AuthorizationAPI/Authorization.Persistence/Repositories/UserRepository.cs
+++ AuthorizationAPI/Authorization.Persistence/Repositories/UserRepository.cs
@@ -12,33 +12,33 @@ internal class UserRepository : IUserRepository
     {
         _db = db;
     }
-    public async Task<User> CreateAsync(User user, CancellationToken cancellationToken)
+    public async Task<User> CreateAsync(User user, CancellationToken cancellationToken = default)
     {
         _db.Users.Add(user);
         await _db.SaveChangesAsync();
         return user;
     }
 
-    public async Task DeleteAsync(User user, CancellationToken cancellationToken)
+    public async Task DeleteAsync(User user, CancellationToken cancellationToken = default)
     {
         _db.Users.Remove(user);
         await _db.SaveChangesAsync();
     }
 
-    public async Task<User> FindByEmailAsync(string email, CancellationToken cancellationToken)
+    public async Task<User> FindByEmailAsync(string email, CancellationToken cancellationToken = default)
     {
         return await _db.Users
             .AsNoTracking()
             .FirstOrDefaultAsync(x => x.Email == email);
     }
 
-    public async Task<User> GetAsync(Guid id, CancellationToken cancellationToken)
+    public async Task<User> GetAsync(Guid id, CancellationToken cancellationToken = default)
     {
         return await _db.Users
             .FirstOrDefaultAsync(x => x.Id == id);
     }
 
-    public async Task<User> UpdateAsync(User user, CancellationToken cancellationToken)
+    public async Task<User> UpdateAsync(User user, CancellationToken cancellationToken = default)
     {
         _db.Users.Update(user);
         await _db.SaveChangesAsync();
