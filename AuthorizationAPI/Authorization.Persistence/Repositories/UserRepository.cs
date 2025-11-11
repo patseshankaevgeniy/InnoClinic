3b35163d78fa57d3f82470f18429b7e151f1480c
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2025-11-11 10:52:33 +0300

    update UserRepository

--- AuthorizationAPI/Authorization.Persistence/Repositories/UserRepository.cs
+++ AuthorizationAPI/Authorization.Persistence/Repositories/UserRepository.cs
@@ -7,42 +7,40 @@ namespace Authorization.Persistence.Repositories;
 internal class UserRepository : IUserRepository
 {
     private readonly ApplicationDbContext _db;
-    private readonly DbSet<User> _users;
 
     public UserRepository(ApplicationDbContext db)
     {
         _db = db;
-        _users = _db.Set<User>();
     }
     public async Task<User> CreateAsync(User user, CancellationToken cancellationToken)
     {
-        _users.Add(user);
+        _db.Users.Add(user);
         await _db.SaveChangesAsync();
         return user;
     }
 
     public async Task DeleteAsync(User user, CancellationToken cancellationToken)
     {
-        _users.Remove(user);
+        _db.Users.Remove(user);
         await _db.SaveChangesAsync();
     }
 
     public async Task<User> FindByEmailAsync(string email, CancellationToken cancellationToken)
     {
-        return await _users
+        return await _db.Users
             .AsNoTracking()
             .FirstOrDefaultAsync(x => x.Email == email);
     }
 
     public async Task<User> GetAsync(Guid id, CancellationToken cancellationToken)
     {
-        return await _users
+        return await _db.Users
             .FirstOrDefaultAsync(x => x.Id == id);
     }
 
     public async Task<User> UpdateAsync(User user, CancellationToken cancellationToken)
     {
-        _users.Update(user);
+        _db.Users.Update(user);
         await _db.SaveChangesAsync();
 
         return user;
