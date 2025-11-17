9e9a6ddbaa90d504bea21b36ec7dde08d377780c
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2025-11-17 11:17:45 +0300

    update configuration path

--- AuthorizationAPI/AuthorizationAPI/Authorization.BLL/Services/JwtTokenService.cs
+++ AuthorizationAPI/AuthorizationAPI/Authorization.BLL/Services/JwtTokenService.cs
@@ -17,7 +17,7 @@ public sealed class JwtTokenService(IConfiguration configuration) : IJwtTokenSer
             new Claim(ClaimTypes.NameIdentifier, userId.ToString())
         };
 
-        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Auth_Key"]!));
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Auth:Key"]!));
         var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
         var tokenDescriptor = new JwtSecurityToken(
             issuer: configuration["Auth:Issuer"],
