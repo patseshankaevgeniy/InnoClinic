571777f5638566212f536853a1b3d7aabbf68ae8
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2025-11-24 11:13:47 +0300

    Add OfficesApi with OfficeEntity

---
+++ AuthorizationAPI/AuthorizationAPI/Offices.DAL/Entities/Office.cs
@@ -0,0 +1,13 @@
+﻿namespace Offices.DAL.Entities
+{
+    public class Office
+    {
+        public required Guid Id { get; set; }
+        public required string Address { get; set; }
+        public required string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+        public required Guid PhotoId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
