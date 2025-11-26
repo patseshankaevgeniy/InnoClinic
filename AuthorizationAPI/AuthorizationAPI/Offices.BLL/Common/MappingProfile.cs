6263d7e3f3a47b809e6f6e2737e7f8c86d2c53e3
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2025-11-26 11:04:33 +0300

    Add OfficesService with AutoMapper

---
+++ AuthorizationAPI/AuthorizationAPI/Offices.BLL/Common/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Offices.BLL.Models;
+using Offices.DAL.Entities;
+
+namespace Offices.BLL.Common;
+
+public sealed class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Office, OfficeModel>().ReverseMap();
+    }
+}
---
+++ AuthorizationAPI/AuthorizationAPI/Offices.BLL/DI/BusinessLayerServiceRegister.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Offices.BLL.Services;
+using Offices.BLL.Services.Interfaces;
+using Offices.DAL.DI;
+using System.Reflection;
+
+namespace Offices.BLL.DI;
+
+public static class BusinessLayerServiceRegister
+{
+    public static IServiceCollection RegisterBusinessLayerDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.RegisterDataLayerDependencies(configuration);
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddScoped<IOfficesService, OfficesService>();
+
+        return services;
+    }
+}
