02a287256ff1f56319d4604741d71cd8c13ac415
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2025-11-26 12:04:38 +0300

    add exceptions

---
+++ AuthorizationAPI/AuthorizationAPI/Offices.BLL/Common/Exceptions/BadRequestException.cs
@@ -0,0 +1,18 @@
+﻿namespace Offices.BLL.Common.Exceptions;
+
+public sealed class BadRequestException : Exception
+{
+    public BadRequestException()
+    {
+    }
+
+    public BadRequestException(string message)
+        : base(message)
+    {
+    }
+
+    public BadRequestException(string message, Exception exception)
+        : base(message, exception)
+    {
+    }
+}
