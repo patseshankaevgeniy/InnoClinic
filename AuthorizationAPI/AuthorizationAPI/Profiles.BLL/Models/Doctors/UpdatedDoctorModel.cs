73cdb1c8e8e2b9abc1fd79be500b3ffd0d09129d
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2025-12-04 14:23:49 +0300

    Add ProfilesAPI template with entities

---
+++ AuthorizationAPI/AuthorizationAPI/Profiles.BLL/Models/Doctors/UpdatedDoctorModel.cs
@@ -0,0 +1,11 @@
+﻿using Profiles.BLL.Models.Specializations;
+using Profiles.DAL.Enums;
+
+namespace Profiles.BLL.Models.Doctors;
+
+public class UpdatedDoctorModel
+{
+    public required string PhoneNumber { get; set; }
+    public DoctorStatus Status { get; set; }
+    public SpecializationModel Specialization { get; set; }
+}
