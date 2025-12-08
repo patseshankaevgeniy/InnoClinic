4db6b08e3c19374819188646b61ea3ab4e4d872f
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2025-12-08 09:53:54 +0300

    remove line

--- AuthorizationAPI/AuthorizationAPI/Profiles.DAL/Entities/Doctor.cs
+++ AuthorizationAPI/AuthorizationAPI/Profiles.DAL/Entities/Doctor.cs
@@ -6,7 +6,6 @@ public class Doctor : BaseEntity
 {
     public DoctorStatus Status { get; set; }
     public DateTime CareerStartAt { get; set; }
-
     public Guid SpecializationId { get; set; }
 
     public Specialization Specialization { get; set; }
