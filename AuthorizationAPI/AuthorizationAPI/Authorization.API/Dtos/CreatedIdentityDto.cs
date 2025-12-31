4d2bf6d5a5f05fb989a4d5f2c46630b8940a3ff9
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2025-12-31 14:31:53 +0300

    add required param

--- AuthorizationAPI/AuthorizationAPI/Authorization.API/Dtos/CreatedIdentityDto.cs
+++ AuthorizationAPI/AuthorizationAPI/Authorization.API/Dtos/CreatedIdentityDto.cs
@@ -9,5 +9,5 @@ public sealed class CreatedIdentityDto
     public required string LastName { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
-    public UserRole Role { get; set; }
+    public required UserRole Role { get; set; }
 }
