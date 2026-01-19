feeeac4c00a16d222da4f43c306011110b2f7f20
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2026-01-19 16:35:15 +0300

    add primary constructor

--- AuthorizationAPI/AuthorizationAPI/DocumentsApi/Documents.DAL/Repositories/DocumentsRepository.cs
+++ AuthorizationAPI/AuthorizationAPI/DocumentsApi/Documents.DAL/Repositories/DocumentsRepository.cs
@@ -4,26 +4,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Documents.DAL.Repositories;
 
-public class DocumentRepository : IDocumentRepository
+public class DocumentRepository(ApplicationDbContext context) : IDocumentRepository
 {
-    private readonly ApplicationDbContext _context;
-    
-    public DocumentRepository(ApplicationDbContext context) => _context = context;
-
     public async Task<Document?> GetByIdAsync(Guid id, CancellationToken cancellationToken) =>
-        await _context.Documents.AsNoTracking().FirstOrDefaultAsync(x=>x.Id == id, cancellationToken);
+        await context.Documents.AsNoTracking().FirstOrDefaultAsync(x=>x.Id == id, cancellationToken);
 
     public async Task<IEnumerable<Document>> GetByIdentityIdAsync(Guid identityId, CancellationToken cancellationToken) =>
-        await _context.Documents
+        await context.Documents
                       .AsNoTracking()
                       .Where(d => d.IdentityId == identityId)
                       .ToListAsync(cancellationToken);
 
     public async Task AddAsync(Document document, CancellationToken cancellationToken) =>
-        await _context.Documents.AddAsync(document, cancellationToken);
+        await context.Documents.AddAsync(document, cancellationToken);
 
-    public void Delete(Document document) => _context.Documents.Remove(document);
+    public void Delete(Document document) => context.Documents.Remove(document);
 
     public async Task SaveChangesAsync(CancellationToken cancellationToken) =>
-        await _context.SaveChangesAsync(cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
 }
